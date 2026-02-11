9b1685c57201b04f15554132fd9b5ffdeea8bfec
Suhana <suhanabagwan39@gmail.com>
2026-02-11 10:22:49 +0530

    StaticDemo.java

---
+++ EXP2/StaticDemo.java
@@ -0,0 +1,17 @@
+public class StaticDemo {
+
+    static int count;
+
+    static {
+        count = 10;
+        System.out.println("Static Block Executed");
+    }
+
+    static void display() {
+        System.out.println("Count = " + count);
+    }
+
+    public static void main(String args[]) {
+        display();
+    }
+}
